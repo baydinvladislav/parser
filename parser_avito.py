ed19680aa27c0dc14e3c20b45009573608160361
Vladislav Baydin <baydinvladislav@yandex.ru>
2020-09-19 14:00:01 +0300

    working version parser_avito

---
+++ parser_avito.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.avito.ru/moskva/avtomobili/bmw-ASgBAgICAUTgtg3klyg?cd=1'
+HEADERS = {
+    'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
+    'accept':'*/*'
+}
+DOMAIN = 'https://www.avito.ru'
+
+
+def get_html(url, params=None):
+    response = requests.get(url, headers=HEADERS, params=params)
+    return response
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='item__line')
+
+    cars = []
+    for item in items:
+        cars.append({
+            'title': item.find('a', class_='snippet-link').get_text(strip=True),
+            'link': DOMAIN + item.find('a', class_='snippet-link').get('href'),
+            'price': item.find('div', class_='snippet-price-row').get_text(strip='True'),
+            'station_metro': item.find('span', class_='item-address-georeferences-item__content').get_text(strip='True'),
+        })
+    print(cars)
+
+
+
+def parse():
+    html = get_html(URL)
+    if html.status_code == 200:
+        get_content(html.text)
+    else:
+        print('Не удалось получить ответ от web-страницы')
+
+
+parse()
